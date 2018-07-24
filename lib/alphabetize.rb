ef425104c06398101d9fc78062721cf01343b473
Felix Chan <felixchan29@gmail.com>
2018-07-24 04:17:58 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_map = {}
+  
+  for i in 0..esperanto_alphabet.length-1 do 
+    esp_map[esperanto_alphabet[i]] = i
+  end
+  
+  arr.sort_by do |word| 
+    word.chars.map do |c| 
+      esp_map[c]
+    end
+  end
+  
 end
