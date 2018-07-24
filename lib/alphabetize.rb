0d4d6107130f6565c3506463b747129f71515a9d
Felix Chan <felixchan29@gmail.com>
2018-07-24 03:50:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,11 @@
 def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  esp_map = esperanto_alphabet.map
+  esp_map = {}
+  
+  esperanto_alphabet.each_with_index do |letter, index|
+    esp_map[letter] = index
+  end
+  
   arr.sort_by {|word| word}
 end
