2162c7b6738e8f25fd7d4e8abe0b2a1262c8ac67
Felix Chan <felixchan29@gmail.com>
2018-07-24 03:56:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,8 +4,7 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esp_map = {}
   
-  for i in esperanto_alphabet.length-1 do 
-    binding.pry
+  for i in 0..esperanto_alphabet.length-1 do 
     esp_map[esperanto_alphabet[i]] = i
   end
   
